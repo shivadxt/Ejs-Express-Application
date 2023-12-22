ff08e2e2182da09a96587adb1efcab31e0f4ddc2
shivadxt <shivamweb3.0@gmail.com>
2023-12-22 11:51:17 +0530

    Documentation added

--- app.js
+++ app.js
@@ -22,9 +22,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Setting up session 
 app.use(session({
-  resave:false, //If value of session doesn't change don't save again and again.
-  saveUninitialized:false, //Not saving useless information that is not required
-  secret:"Hello There" //Secret key is the string through which we'll encrypt the data
+  resave:false, 
+  //Takes a Boolean value. It enables the session to be stored back to the session store, 
+  //even if the session was never modified during the request. 
+  //This can result in a race situation in case a client makes two parallel requests to the server. 
+  saveUninitialized:false, 
+  //Not saving useless information that is not required
+  //this allows any uninitialized session to be sent to the store. 
+  //When a session is created but not modified, it is referred to as uninitialized.
+  secret:"Hello There" 
+  //a random unique string key used to authenticate a session. 
+  //It is stored in an environment variable and can’t be exposed to the public. 
+  //The key is usually long and randomly generated in a production environment.
 }));
 
 app.use('/', indexRouter);
